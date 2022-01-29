8bba2274b50203bfe516ba55939f749a9b598fc8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-30 00:40:03 +0100

    Add test function for full text of effects.

---
+++ test/effect/full_text.py
@@ -0,0 +1,16 @@
+from colorist import effect_bold, effect_dim, effect_underline, effect_blink, effect_reverse, effect_hide
+
+if __name__ == "__main__":
+    print("")
+    effect_bold("This is BOLD!")
+    print("")
+    effect_dim("This is DIMMED!")
+    print("")
+    effect_underline("This is UNDERLINED!")
+    print("")
+    effect_blink("This is BLINKING!")
+    print("")
+    effect_reverse("This is REVERSED!")
+    print("")
+    effect_hide("This is HIDDEN!")
+    print("")
