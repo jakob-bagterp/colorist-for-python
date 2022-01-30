e4650ace7afb1594ebd38d7a826445eec211f623
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-30 12:03:43 +0100

    Refactor and split test examples into groups for standard color, bright color, effect.

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,14 @@
 import subprocess
 
-def run_terminal_examples() -> None:
+def run_terminal_examples_for_color() -> None:
     subprocess.call("python3 ./test/color/examples_full_text.py".split())
     subprocess.call("python3 ./test/color/examples_custom_text.py".split())
+    
+def run_terminal_examples_for_bright_color() -> None:
     subprocess.call("python3 ./test/bright_color/examples_full_text.py".split())
     subprocess.call("python3 ./test/bright_color/examples_custom_text.py".split())
+
+def run_terminal_examples_for_effect() -> None:
     subprocess.call("python3 ./test/effect/examples_full_text.py".split())
     subprocess.call("python3 ./test/effect/examples_full_text_with_color.py".split())
     subprocess.call("python3 ./test/effect/examples_custom_text.py".split())
@@ -13,5 +17,7 @@ def run_pytest() -> None:
     subprocess.call("pytest".split())
 
 if __name__ == "__main__":
-    run_terminal_examples()
+    run_terminal_examples_for_color()
+    run_terminal_examples_for_bright_color()
+    run_terminal_examples_for_effect()
     run_pytest()
