6bef8d57028e956ba9db6e0e94516adb3b41b511
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-30 11:59:36 +0100

    Add example of effect with color to test examples.

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,7 @@ def run_terminal_examples() -> None:
     subprocess.call("python3 ./test/color/examples_full_text.py".split())
     subprocess.call("python3 ./test/bright_color/examples_full_text.py".split())
     subprocess.call("python3 ./test/effect/examples_full_text.py".split())
+    subprocess.call("python3 ./test/effect/examples_full_text_with_color.py".split())
 
 def run_pytest() -> None:
     subprocess.call("pytest".split())
