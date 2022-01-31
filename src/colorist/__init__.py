a9690472c8c0c1b0e4992076b16f6fb882daafd2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-31 02:13:07 +0100

    Add background color functions.

--- src/colorist/__init__.py
+++ src/colorist/__init__.py
@@ -63,6 +63,54 @@ def bright_white(text: str) -> None:
 
 def bright_black(text: str) -> None:
     print_color(text, BrightColor.BLACK)
+    
+def bg_green(text: str) -> None:
+    print_color(text, bgcolor = BgColor.GREEN)
+
+def bg_yellow(text: str) -> None:
+    print_color(text, bgcolor = BgColor.YELLOW)
+
+def bg_red(text: str) -> None:
+    print_color(text, bgcolor = BgColor.RED)
+
+def bg_magenta(text: str) -> None:
+    print_color(text, bgcolor = BgColor.MAGENTA)
+
+def bg_blue(text: str) -> None:
+    print_color(text, bgcolor = BgColor.BLUE)
+
+def bg_cyan(text: str) -> None:
+    print_color(text, bgcolor = BgColor.CYAN)
+
+def bg_white(text: str) -> None:
+    print_color(text, bgcolor = BgColor.WHITE)
+
+def bg_black(text: str) -> None:
+    print_color(text, bgcolor = BgColor.BLACK)
+
+def bg_bright_green(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.GREEN)
+
+def bg_bright_yellow(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.YELLOW)
+
+def bg_bright_red(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.RED)
+
+def bg_bright_magenta(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.MAGENTA)
+
+def bg_bright_blue(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.BLUE)
+
+def bg_bright_cyan(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.CYAN)
+
+def bg_bright_white(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.WHITE)
+
+def bg_bright_black(text: str) -> None:
+    print_color(text, bgcolor = BgBrightColor.BLACK)
 
 def print_effect(text: str, effect: Effect, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
     print(f"{color}{effect}{text}{Effect.OFF}")
