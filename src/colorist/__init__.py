766de503d98f87fc8d458d7243828d9d087c21be
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-31 22:51:44 +0100

    Minor reorder of imports.

--- src/colorist/__init__.py
+++ src/colorist/__init__.py
@@ -1,7 +1,5 @@
 __all__ = ["model"]
 
-from .version import __version__
-
 from typing import Union
 from . import model
 from .model.color import Color
@@ -9,6 +7,7 @@ from .model.bright_color import BrightColor
 from .model.background_color import BgColor
 from .model.background_bright_color import BgBrightColor
 from .model.effect import Effect
+from .version import __version__
 
 def print_color(text: str, color: Union[Color, BrightColor, str, None] = None, bgcolor: Union[BgColor, BgBrightColor, str, None] = None) -> None:
     if color is None:
