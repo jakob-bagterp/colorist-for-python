8f9773636c5fa8611fa8410a44eb1d0e921f8fd2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-31 16:49:04 +0100

    Reorder test example functions (no functional changes).

--- run_tests.py
+++ run_tests.py
@@ -8,11 +8,6 @@ def run_terminal_examples_for_bright_color() -> None:
     subprocess.call("python3 ./test/bright_color/examples_full_text.py".split())
     subprocess.call("python3 ./test/bright_color/examples_custom_text.py".split())
 
-def run_terminal_examples_for_effect() -> None:
-    subprocess.call("python3 ./test/effect/examples_full_text.py".split())
-    subprocess.call("python3 ./test/effect/examples_full_text_with_color.py".split())
-    subprocess.call("python3 ./test/effect/examples_custom_text.py".split())
-
 def run_terminal_examples_for_background_color() -> None:
     subprocess.call("python3 ./test/background_color/examples_full_text.py".split())
     subprocess.call("python3 ./test/background_color/examples_custom_text.py".split())
@@ -20,6 +15,11 @@ def run_terminal_examples_for_background_color() -> None:
 def run_terminal_examples_for_background_bright_color() -> None:
     subprocess.call("python3 ./test/background_bright_color/examples_full_text.py".split())
     subprocess.call("python3 ./test/background_bright_color/examples_custom_text.py".split())
+
+def run_terminal_examples_for_effect() -> None:
+    subprocess.call("python3 ./test/effect/examples_full_text.py".split())
+    subprocess.call("python3 ./test/effect/examples_full_text_with_color.py".split())
+    subprocess.call("python3 ./test/effect/examples_custom_text.py".split())
 
 def run_pytest() -> None:
     subprocess.call("pytest".split())
