e22fabc16fdcc0fa3a18434a3350e4452352142b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-01 01:44:03 +0100

    Refactor of helper functions into separate module (no functional changes).

--- src/colorist/__init__.py
+++ src/colorist/__init__.py
@@ -1,7 +1,7 @@
-__all__ = ["model"]
+__all__ = ["helper", "model"]
 
 from typing import Union
-from . import model
+from . import helper, model
 from .model.background_bright_color import BgBrightColor
 from .model.background_color import BgColor
 from .model.bright_color import BrightColor
@@ -9,126 +9,116 @@ from .model.color import Color
 from .model.effect import Effect
 from .version import __version__
 
-def print_color(text: str, color: Union[Color, BrightColor, str, None] = None, bgcolor: Union[BgColor, BgBrightColor, str, None] = None) -> None:
-    if color is None:
-        color = ""
-    if bgcolor is None:
-        bgcolor = ""
-    print(f"{bgcolor}{color}{text}{Color.OFF}")
-
 def green(text: str) -> None:
-    print_color(text, Color.GREEN)
+    helper.print_color(text, color = Color.GREEN)
 
 def yellow(text: str) -> None:
-    print_color(text, Color.YELLOW)
+    helper.print_color(text, color = Color.YELLOW)
 
 def red(text: str) -> None:
-    print_color(text, Color.RED)
+    helper.print_color(text, color = Color.RED)
 
 def magenta(text: str) -> None:
-    print_color(text, Color.MAGENTA)
+    helper.print_color(text, color = Color.MAGENTA)
 
 def blue(text: str) -> None:
-    print_color(text, Color.BLUE)
+    helper.print_color(text, color = Color.BLUE)
 
 def cyan(text: str) -> None:
-    print_color(text, Color.CYAN)
+    helper.print_color(text, color = Color.CYAN)
 
 def white(text: str) -> None:
-    print_color(text, Color.WHITE)
+    helper.print_color(text, color = Color.WHITE)
 
 def black(text: str) -> None:
-    print_color(text, Color.BLACK)
+    helper.print_color(text, color = Color.BLACK)
 
 def bright_green(text: str) -> None:
-    print_color(text, BrightColor.GREEN)
+    helper.print_color(text, color = BrightColor.GREEN)
 
 def bright_yellow(text: str) -> None:
-    print_color(text, BrightColor.YELLOW)
+    helper.print_color(text, color = BrightColor.YELLOW)
 
 def bright_red(text: str) -> None:
-    print_color(text, BrightColor.RED)
+    helper.print_color(text, color = BrightColor.RED)
 
 def bright_magenta(text: str) -> None:
-    print_color(text, BrightColor.MAGENTA)
+    helper.print_color(text, color = BrightColor.MAGENTA)
 
 def bright_blue(text: str) -> None:
-    print_color(text, BrightColor.BLUE)
+    helper.print_color(text, color = BrightColor.BLUE)
 
 def bright_cyan(text: str) -> None:
-    print_color(text, BrightColor.CYAN)
+    helper.print_color(text, color = BrightColor.CYAN)
 
 def bright_white(text: str) -> None:
-    print_color(text, BrightColor.WHITE)
+    helper.print_color(text, color = BrightColor.WHITE)
 
 def bright_black(text: str) -> None:
-    print_color(text, BrightColor.BLACK)
+    helper.print_color(text, color = BrightColor.BLACK)
     
 def bg_green(text: str) -> None:
-    print_color(text, bgcolor = BgColor.GREEN)
+    helper.print_color(text, bgcolor = BgColor.GREEN)
 
 def bg_yellow(text: str) -> None:
-    print_color(text, bgcolor = BgColor.YELLOW)
+    helper.print_color(text, bgcolor = BgColor.YELLOW)
 
 def bg_red(text: str) -> None:
-    print_color(text, bgcolor = BgColor.RED)
+    helper.print_color(text, bgcolor = BgColor.RED)
 
 def bg_magenta(text: str) -> None:
-    print_color(text, bgcolor = BgColor.MAGENTA)
+    helper.print_color(text, bgcolor = BgColor.MAGENTA)
 
 def bg_blue(text: str) -> None:
-    print_color(text, bgcolor = BgColor.BLUE)
+    helper.print_color(text, bgcolor = BgColor.BLUE)
 
 def bg_cyan(text: str) -> None:
-    print_color(text, bgcolor = BgColor.CYAN)
+    helper.print_color(text, bgcolor = BgColor.CYAN)
 
 def bg_white(text: str) -> None:
-    print_color(text, bgcolor = BgColor.WHITE)
+    helper.print_color(text, bgcolor = BgColor.WHITE)
 
 def bg_black(text: str) -> None:
-    print_color(text, bgcolor = BgColor.BLACK)
+    helper.print_color(text, bgcolor = BgColor.BLACK)
 
 def bg_bright_green(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.GREEN)
+    helper.print_color(text, bgcolor = BgBrightColor.GREEN)
 
 def bg_bright_yellow(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.YELLOW)
+    helper.print_color(text, bgcolor = BgBrightColor.YELLOW)
 
 def bg_bright_red(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.RED)
+    helper.print_color(text, bgcolor = BgBrightColor.RED)
 
 def bg_bright_magenta(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.MAGENTA)
+    helper.print_color(text, bgcolor = BgBrightColor.MAGENTA)
 
 def bg_bright_blue(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.BLUE)
+    helper.print_color(text, bgcolor = BgBrightColor.BLUE)
 
 def bg_bright_cyan(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.CYAN)
+    helper.print_color(text, bgcolor = BgBrightColor.CYAN)
 
 def bg_bright_white(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.WHITE)
+    helper.print_color(text, bgcolor = BgBrightColor.WHITE)
 
 def bg_bright_black(text: str) -> None:
-    print_color(text, bgcolor = BgBrightColor.BLACK)
-
-def print_effect(text: str, effect: Effect, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
-    print(f"{color}{effect}{text}{Effect.OFF}")
+    helper.print_color(text, bgcolor = BgBrightColor.BLACK)
 
 def effect_bold(text: str, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
-    print_effect(text, Effect.BOLD, color)
+    helper.print_effect(text, Effect.BOLD, color)
 
 def effect_dim(text: str, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
-    print_effect(text, Effect.DIM, color)
+    helper.print_effect(text, Effect.DIM, color)
 
 def effect_underline(text: str, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
-    print_effect(text, Effect.UNDERLINE, color)
+    helper.print_effect(text, Effect.UNDERLINE, color)
 
 def effect_blink(text: str, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
-    print_effect(text, Effect.BLINK, color)
+    helper.print_effect(text, Effect.BLINK, color)
 
 def effect_reverse(text: str, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
-    print_effect(text, Effect.REVERSE, color)
+    helper.print_effect(text, Effect.REVERSE, color)
 
 def effect_hide(text: str, color: Union[Color, BrightColor] = Color.DEFAULT) -> None:
-    print_effect(text, Effect.HIDE, color)
+    helper.print_effect(text, Effect.HIDE, color)
