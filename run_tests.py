265a91ea92407f5bbfbce1597ffce44de1680744
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-19 10:40:28 +0100

    Test: Move example files into separate folder from unit tests.

--- run_tests.py
+++ run_tests.py
@@ -1,25 +1,25 @@
 import subprocess
 
 def run_terminal_examples_for_color() -> None:
-    subprocess.call("python3 ./test/color/examples_full_text.py".split())
-    subprocess.call("python3 ./test/color/examples_custom_text.py".split())
+    subprocess.call("python3 ./test/_example/color/full_text.py".split())
+    subprocess.call("python3 ./test/_example/color/custom_text.py".split())
     
 def run_terminal_examples_for_bright_color() -> None:
-    subprocess.call("python3 ./test/bright_color/examples_full_text.py".split())
-    subprocess.call("python3 ./test/bright_color/examples_custom_text.py".split())
+    subprocess.call("python3 ./test/_example/bright_color/full_text.py".split())
+    subprocess.call("python3 ./test/_example/bright_color/custom_text.py".split())
 
 def run_terminal_examples_for_background_color() -> None:
-    subprocess.call("python3 ./test/background_color/examples_full_text.py".split())
-    subprocess.call("python3 ./test/background_color/examples_custom_text.py".split())
+    subprocess.call("python3 ./test/_example/background_color/full_text.py".split())
+    subprocess.call("python3 ./test/_example/background_color/custom_text.py".split())
 
 def run_terminal_examples_for_background_bright_color() -> None:
-    subprocess.call("python3 ./test/background_bright_color/examples_full_text.py".split())
-    subprocess.call("python3 ./test/background_bright_color/examples_custom_text.py".split())
+    subprocess.call("python3 ./test/_example/background_bright_color/full_text.py".split())
+    subprocess.call("python3 ./test/_example/background_bright_color/custom_text.py".split())
 
 def run_terminal_examples_for_effect() -> None:
-    subprocess.call("python3 ./test/effect/examples_full_text.py".split())
-    subprocess.call("python3 ./test/effect/examples_full_text_with_color.py".split())
-    subprocess.call("python3 ./test/effect/examples_custom_text.py".split())
+    subprocess.call("python3 ./test/_example/effect/full_text.py".split())
+    subprocess.call("python3 ./test/_example/effect/full_text_with_color.py".split())
+    subprocess.call("python3 ./test/_example/effect/custom_text.py".split())
 
 def run_pytest() -> None:
     subprocess.call("pytest".split())
