cfcc3993b54d701310571b8078830bd8e941541d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-03-27 21:41:21 +0200

    Refactor tests and add tests of string concatenation.

--- test/effect/custom_effect_test.py
+++ test/effect/custom_effect_test.py
@@ -2,6 +2,8 @@ import terminal
 
 from colorist import Color, Effect
 
+EFFECT_TEXT = "I want both \033[31mcolored and \033[5mblinking\033[25m text\033[0m inside this paragraph\n"
+
 
 def test_custom_text_effect_1(capfd: object) -> None:
     print(f"I want {Effect.UNDERLINE}underlined text{Effect.UNDERLINE_OFF} inside this paragraph")
@@ -18,4 +20,10 @@ def test_custom_text_effect_2(capfd: object) -> None:
 def test_custom_text_effect_3(capfd: object) -> None:
     print(f"I want both {Color.RED}colored and {Effect.BLINK}blinking{Effect.BLINK_OFF} text{Color.OFF} inside this paragraph")
     terminal_output = terminal.get_output(capfd)
-    assert terminal_output == "I want both \033[31mcolored and \033[5mblinking\033[25m text\033[0m inside this paragraph\n"
+    assert terminal_output == EFFECT_TEXT
+
+
+def test_custom_text_effect_3_string_concatenation(capfd: object) -> None:
+    print("I want both " + Color.RED + "colored and " + Effect.BLINK + "blinking" + Effect.BLINK_OFF + " text" + Color.OFF + " inside this paragraph")
+    terminal_output = terminal.get_output(capfd)
+    assert terminal_output == EFFECT_TEXT
