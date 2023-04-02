45c0a69360659ee56623dda4c8a16ebbc24fd0c0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-02 12:40:34 +0200

    Remove tests and update directory references.

--- test/_example/run_tests.py
+++ test/_example/run_tests.py
@@ -2,33 +2,29 @@ import subprocess
 
 
 def run_terminal_examples_for_color() -> None:
-    subprocess.call("python3 ./test/_example/color/full_text.py".split())
-    subprocess.call("python3 ./test/_example/color/custom_text.py".split())
+    subprocess.call("python3 ./color/full_text.py".split())
+    subprocess.call("python3 ./color/custom_text.py".split())
 
 
 def run_terminal_examples_for_bright_color() -> None:
-    subprocess.call("python3 ./test/_example/bright_color/full_text.py".split())
-    subprocess.call("python3 ./test/_example/bright_color/custom_text.py".split())
+    subprocess.call("python3 ./bright_color/full_text.py".split())
+    subprocess.call("python3 ./bright_color/custom_text.py".split())
 
 
 def run_terminal_examples_for_background_color() -> None:
-    subprocess.call("python3 ./test/_example/background_color/full_text.py".split())
-    subprocess.call("python3 ./test/_example/background_color/custom_text.py".split())
+    subprocess.call("python3 ./background_color/full_text.py".split())
+    subprocess.call("python3 ./background_color/custom_text.py".split())
 
 
 def run_terminal_examples_for_background_bright_color() -> None:
-    subprocess.call("python3 ./test/_example/background_bright_color/full_text.py".split())
-    subprocess.call("python3 ./test/_example/background_bright_color/custom_text.py".split())
+    subprocess.call("python3 ./background_bright_color/full_text.py".split())
+    subprocess.call("python3 ./background_bright_color/custom_text.py".split())
 
 
 def run_terminal_examples_for_effect() -> None:
-    subprocess.call("python3 ./test/_example/effect/full_text.py".split())
-    subprocess.call("python3 ./test/_example/effect/full_text_with_color.py".split())
-    subprocess.call("python3 ./test/_example/effect/custom_text.py".split())
-
-
-def run_pytest() -> None:
-    subprocess.call("pytest".split())
+    subprocess.call("python3 ./effect/full_text.py".split())
+    subprocess.call("python3 ./effect/full_text_with_color.py".split())
+    subprocess.call("python3 ./effect/custom_text.py".split())
 
 
 if __name__ == "__main__":
@@ -37,4 +33,3 @@ if __name__ == "__main__":
     run_terminal_examples_for_background_color()
     run_terminal_examples_for_background_bright_color()
     run_terminal_examples_for_effect()
-    run_pytest()
