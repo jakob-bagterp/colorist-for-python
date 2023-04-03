7e301f50cf6097669959768feba07c3b934695c9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-03 23:26:29 +0200

    Add helper functions to generate ANSI color codes.

---
+++ src/colorist/helper/generate.py
@@ -0,0 +1,15 @@
+from ..constants.ansi import SGR_END, AnsiColor, AnsiColorSelector
+from ..constants.ascii import AsciiEscapeCode
+
+
+def control_sequence_inducer(ascii_escape_code: AsciiEscapeCode) -> str:
+    """Control Sequence Inducer (CSI) marks the beginning of a control sequence, e.g. "\\u1b[31m"."""
+
+    return f"{ascii_escape_code}["
+
+
+def ansi_standard_color_sequence(ascii_escape_code: AsciiEscapeCode, selector: AnsiColorSelector, color: AnsiColor) -> str:
+    """Generate standard ANSI color sequence, e.g. "\\u1b[31m"."""
+
+    csi = control_sequence_inducer(ascii_escape_code)
+    return f"{csi}{selector}{color}{SGR_END}"
