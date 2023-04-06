839e4ff08124a662e571885c508f1c26805aacbc
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-06 14:35:02 +0200

    Add tests of validation helper methods for HSL.

---
+++ test/helper/validate/is_valid_hsl_hue_test.py
@@ -0,0 +1,17 @@
+import pytest
+
+from colorist import helper
+
+
+@pytest.mark.parametrize("input, expected", [
+    (0, True),
+    (100, True),
+    (360, True),
+    (0.0, True),
+    (360.0, True),
+    (360.1, False),
+    (361, False),
+    (-1, False),
+])
+def test_is_valid_hsl_hue(input: float, expected: bool) -> None:
+    assert helper.validate.is_valid_hsl_hue(input) is expected
