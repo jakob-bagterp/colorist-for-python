f0e1f92fad610e7d0aa039a7911c9433905295ed
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-06 11:53:12 +0200

    Add OFF option to RGB color base class.

--- src/colorist/model/abc/rgb.py
+++ src/colorist/model/abc/rgb.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from ...constants.ansi import RESET_ALL
 from ...helper.error import message_for_rgb_value_error
 from ...helper.validate import is_valid_rgb_value
 
@@ -7,7 +8,7 @@ from ...helper.validate import is_valid_rgb_value
 class RGB_ABC(ABC):
     """Abstract base class for RGB color classes."""
 
-    __slots__ = ["red", "green", "blue", "_ansi_code"]
+    __slots__ = ["red", "green", "blue", "OFF", "_ansi_code"]
 
     def __init__(self, red: int, green: int, blue: int) -> None:
         if not is_valid_rgb_value(red):
@@ -20,7 +21,9 @@ class RGB_ABC(ABC):
         self.red: int = red
         self.green: int = green
         self.blue: int = blue
+
         self._ansi_code: str = self.generate_ansi_code()
+        self.OFF = RESET_ALL
 
     def __str__(self) -> str:
         return self._ansi_code
