8a5eaa352d911b2fc4199dd726648428e897df9d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-06 11:12:04 +0200

    Refactor test for effects, including increased coverage.

--- test/effect/custom_effect_test.py
+++ test/effect/custom_effect_test.py
@@ -1,3 +1,4 @@
+import pytest
 import terminal
 
 from colorist import Color, Effect
@@ -5,25 +6,22 @@ from colorist import Color, Effect
 EFFECT_TEXT = "I want both \033[31mcolored and \033[5mblinking\033[25m text\033[0m inside this paragraph\n"
 
 
-def test_custom_text_effect_1(capfd: object) -> None:
-    print(f"I want {Effect.UNDERLINE}underlined text{Effect.UNDERLINE_OFF} inside this paragraph")
+@pytest.mark.parametrize("text, expected", [
+    # F-strings and all effect options:
+    (f"Only effect for this {Effect.BOLD}word{Effect.BOLD_OFF}.", "Only effect for this \033[1mword\033[21m.\n"),
+    (f"Only effect for this {Effect.DIM}word{Effect.DIM_OFF}.", "Only effect for this \033[2mword\033[22m.\n"),
+    (f"Only effect for this {Effect.UNDERLINE}word{Effect.UNDERLINE_OFF}.", "Only effect for this \033[4mword\033[24m.\n"),
+    (f"Only effect for this {Effect.BLINK}word{Effect.BLINK_OFF}.", "Only effect for this \033[5mword\033[25m.\n"),
+    (f"Only effect for this {Effect.REVERSE}word{Effect.REVERSE_OFF}.", "Only effect for this \033[7mword\033[27m.\n"),
+    (f"Only effect for this {Effect.HIDE}word{Effect.HIDE_OFF}.", "Only effect for this \033[8mword\033[28m.\n"),
+
+    # String concatenation:
+    ("I want both " + Color.RED + "colored and " + Effect.BLINK + "blinking" + Effect.BLINK_OFF + " text" + Color.OFF + " inside this paragraph", EFFECT_TEXT),
+
+    # Multiple and mixed parameters inside string:
+    (f"I want both {Color.RED}colored and {Effect.BLINK}blinking{Effect.BLINK_OFF} text{Color.OFF} inside this paragraph", EFFECT_TEXT),
+])
+def test_custom_text_effect(text: str, expected: str, capfd: object) -> None:
+    print(text)
     terminal_output = terminal.get_output(capfd)
-    assert terminal_output == "I want \033[4munderlined text\033[24m inside this paragraph\n"
-
-
-def test_custom_text_effect_2(capfd: object) -> None:
-    print(f"I want {Effect.BOLD}emphasized text{Effect.BOLD_OFF} inside this paragraph")
-    terminal_output = terminal.get_output(capfd)
-    assert terminal_output == "I want \033[1memphasized text\033[21m inside this paragraph\n"
-
-
-def test_custom_text_effect_3(capfd: object) -> None:
-    print(f"I want both {Color.RED}colored and {Effect.BLINK}blinking{Effect.BLINK_OFF} text{Color.OFF} inside this paragraph")
-    terminal_output = terminal.get_output(capfd)
-    assert terminal_output == EFFECT_TEXT
-
-
-def test_custom_text_effect_3_string_concatenation(capfd: object) -> None:
-    print("I want both " + Color.RED + "colored and " + Effect.BLINK + "blinking" + Effect.BLINK_OFF + " text" + Color.OFF + " inside this paragraph")
-    terminal_output = terminal.get_output(capfd)
-    assert terminal_output == EFFECT_TEXT
+    assert terminal_output == expected
