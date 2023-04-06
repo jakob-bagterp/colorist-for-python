95aca389ca3b0750ecba8b89cf9349f6107e0378
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-06 10:29:05 +0200

    Refactor tests of custom text to use Pytest parameters instead of individual functions, including increased coverage.

--- test/background/bright_color/custom_background_bright_color_test.py
+++ test/background/bright_color/custom_background_bright_color_test.py
@@ -1,17 +1,28 @@
+import pytest
 import terminal
 
 from colorist import BgBrightColor
 
-CYAN_BG_TEXT = "I want \033[106mcyan\033[0m background color inside this paragraph\n"
 
+@pytest.mark.parametrize("text, expected", [
+    # F-strings and all color options:
+    (f"Only color this {BgBrightColor.BLACK}word{BgBrightColor.OFF}.", "Only color this \033[100mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.RED}word{BgBrightColor.OFF}.", "Only color this \033[101mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.GREEN}word{BgBrightColor.OFF}.", "Only color this \033[102mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.YELLOW}word{BgBrightColor.OFF}.", "Only color this \033[103mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.BLUE}word{BgBrightColor.OFF}.", "Only color this \033[104mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.MAGENTA}word{BgBrightColor.OFF}.", "Only color this \033[105mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.CYAN}word{BgBrightColor.OFF}.", "Only color this \033[106mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.WHITE}word{BgBrightColor.OFF}.", "Only color this \033[107mword\033[0m.\n"),
+    (f"Only color this {BgBrightColor.DEFAULT}word{BgBrightColor.OFF}.", "Only color this \033[109mword\033[0m.\n"),
 
-def test_custom_text_background_color_1(capfd: object) -> None:
-    print(f"I want {BgBrightColor.CYAN}cyan{BgBrightColor.OFF} background color inside this paragraph")
+    # String concatenation:
+    ("I want " + BgBrightColor.RED + "red" + BgBrightColor.OFF + " color inside this paragraph", "I want \033[101mred\033[0m color inside this paragraph\n"),
+
+    # Multiple and mixed parameters inside string:
+    (f"Both {BgBrightColor.GREEN}green{BgBrightColor.OFF} and {BgBrightColor.YELLOW}yellow{BgBrightColor.OFF} are nice colors", "Both \033[102mgreen\033[0m and \033[103myellow\033[0m are nice colors\n"),
+])
+def test_custom_text_background_bright_color(text: str, expected: str, capfd: object) -> None:
+    print(text)
     terminal_output = terminal.get_output(capfd)
-    assert terminal_output == CYAN_BG_TEXT
-
-
-def test_custom_text_background_color_1_string_concatenation(capfd: object) -> None:
-    print("I want " + BgBrightColor.CYAN + "cyan" + BgBrightColor.OFF + " background color inside this paragraph")
-    terminal_output = terminal.get_output(capfd)
-    assert terminal_output == CYAN_BG_TEXT
+    assert terminal_output == expected
