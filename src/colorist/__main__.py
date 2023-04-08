e0a0698714bb7f07df48eaa9773a462f7abda83a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-08 12:35:01 +0200

    Refactor and split print standard background color functions into separate file.

--- src/colorist/__main__.py
+++ src/colorist/__main__.py
@@ -1,41 +1,8 @@
 from . import helper
 from .model.background_bright_color import BgBrightColor
-from .model.background_color import BgColor
 from .model.bright_color import BrightColor
 from .model.color import Color
 from .model.effect import Effect
-
-
-def bg_green(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.GREEN)
-
-
-def bg_yellow(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.YELLOW)
-
-
-def bg_red(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.RED)
-
-
-def bg_magenta(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.MAGENTA)
-
-
-def bg_blue(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.BLUE)
-
-
-def bg_cyan(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.CYAN)
-
-
-def bg_white(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.WHITE)
-
-
-def bg_black(text: str) -> None:
-    helper.print.color(text, bg_color=BgColor.BLACK)
 
 
 def bg_bright_green(text: str) -> None:
---
+++ src/colorist/print/background/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["color"]
+
+from . import color
