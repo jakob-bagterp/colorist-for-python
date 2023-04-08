6fcb9cdd2ee90b1466bdb2d17593794ba35bbad2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-08 12:55:31 +0200

    Update __init__ files.

--- src/colorist/model/__init__.py
+++ src/colorist/model/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ["BgBrightColor", "BgColor", "BgColorRGB", "BrightColor", "Color", "ColorRGB", "Effect"]
+__all__ = ["BgBrightColor", "BgColor", "BgColorRGB", "BrightColor", "foreground", "ColorRGB", "Effect"]
 
+from . import foreground
 from .background_bright_color import BgBrightColor
 from .background_color import BgColor
 from .bright_color import BrightColor
 from .effect import Effect
-from .foreground.color import Color
 from .rgb import BgColorRGB, ColorRGB
