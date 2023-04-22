64e9b3b0a156aa24531469b68460f4442d2e6aef
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-22 12:17:23 +0200

    Add Hex text color examples.

---
+++ test/_examples/hex/full_text.py
@@ -0,0 +1,9 @@
+# Copyright 2022 – present, Jakob Bagterp. BSD 3-Clause license and refer to LICENSE file.
+
+from colorist import bg_hex, hex
+
+if __name__ == "__main__":
+    print("")
+    hex("I want this text in coral Hex colors", "#ff7f50")
+    bg_hex("I want this background in coral Hex colors", "#ff7f50")
+    print("")
