e9dd9b2877c75480b3f6f581a9cc6e06a86d3155
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-22 10:09:19 +0200

    Refactor test of __repr__ for ColorHSL and add extra tests of BgColorHSL.

--- test/model/hsl/hsl_repr_test.py
+++ test/model/hsl/hsl_repr_test.py
@@ -2,14 +2,22 @@
 
 import pytest
 
-from colorist import ColorHSL
+from colorist import BgColorHSL, ColorHSL
 
-
-@pytest.mark.parametrize("hsl, expected_repr", [
+MOCK_DATASET = [
     ((0, 0, 0), "H: 0, S: 0, L: 0"),
     ((137, 24, 82), "H: 137, S: 24, L: 82"),
     ((360, 100, 100), "H: 360, S: 100, L: 100"),
-])
+]
+
+
+@pytest.mark.parametrize("hsl, expected_repr", MOCK_DATASET)
 def test_repr_of_color_hsl(hsl: tuple[int, int, int], expected_repr: str) -> None:
     color_hsl = ColorHSL(*hsl)
+    assert repr(color_hsl) == expected_repr
+
+
+@pytest.mark.parametrize("hsl, expected_repr", MOCK_DATASET)
+def test_repr_of_background_color_hsl(hsl: tuple[int, int, int], expected_repr: str) -> None:
+    color_hsl = BgColorHSL(*hsl)
     assert repr(color_hsl) == expected_repr
