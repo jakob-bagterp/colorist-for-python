cc2449b2131de4373c0d078dea20022ae03d86ee
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-24 00:54:24 +0200

    Refactor RGB print text color function to use ColorRGB class with built-in input validation and use general print_color() method.

--- src/colorist/print/foreground/rgb.py
+++ src/colorist/print/foreground/rgb.py
@@ -1,9 +1,11 @@
 # Copyright 2022 – present, Jakob Bagterp. BSD 3-Clause license and refer to LICENSE file.
 
-from ... import helper
+from ...model.foreground.rgb import ColorRGB
+from ...print.general import print_color
 
 
 def rgb(text: str, red: int, green: int, blue: int) -> None:
     """Prints full line of custom RGB-colored text. Values for red, green, blue can be between 0 and 255."""
 
-    helper.print.rgb(text, red, green, blue)
+    color_rgb = ColorRGB(red, green, blue)
+    print_color(text, color=color_rgb)
