0d4f8613f7a01b00f4221915d880a77b226b4249
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-24 00:55:13 +0200

    Refactor RGB print colored background function to use BgColorRGB class with built-in input validation and use general print_color() method.

--- src/colorist/print/background/rgb.py
+++ src/colorist/print/background/rgb.py
@@ -1,9 +1,11 @@
 # Copyright 2022 – present, Jakob Bagterp. BSD 3-Clause license and refer to LICENSE file.
 
-from ... import helper
+from ...model.background.rgb import BgColorRGB
+from ...print.general import print_color
 
 
 def bg_rgb(text: str, red: int, green: int, blue: int) -> None:
     """Prints full line of text on custom RGB-colored background. Values for red, green, blue can be between 0 and 255."""
 
-    helper.print.bg_rgb(text, red, green, blue)
+    bg_color_rgb = BgColorRGB(red, green, blue)
+    print_color(text, bg_color=bg_color_rgb)
