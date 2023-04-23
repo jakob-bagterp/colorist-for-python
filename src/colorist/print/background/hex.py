d69fc291956689d0070fcfed570caf49395e30cf
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-04-23 19:00:25 +0200

    Refactor Hex print colored background function to use BgColorHex class with built-in input validation and use general print_color() method.

--- src/colorist/print/background/hex.py
+++ src/colorist/print/background/hex.py
@@ -1,10 +1,11 @@
 # Copyright 2022 – present, Jakob Bagterp. BSD 3-Clause license and refer to LICENSE file.
 
-from ... import helper
+from ...model.background.hex import BgColorHex
+from ...print.general import print_color
 
 
 def bg_hex(text: str, hex: str) -> None:
     """Prints full line of text on colored background defined in Hex color. Expects valid Hex color value, for instance #B4FBB8 or B4FBB8, #B4F or B4F."""
 
-    red, green, blue = helper.convert.hex_to_rgb(hex)
-    helper.print.bg_rgb(text, red, green, blue)
+    bg_color_hex = BgColorHex(hex)
+    print_color(text, bg_color=bg_color_hex)
