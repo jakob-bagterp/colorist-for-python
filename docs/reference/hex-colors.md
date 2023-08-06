aceccff6d39f89809f0fb33eb27810173b3092a3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-08-06 17:52:29 +0200

    Add Hex and HSL full text functions to references, including documentation wrapper functions.

--- docs/reference/hex-colors.md
+++ docs/reference/hex-colors.md
@@ -8,6 +8,20 @@ tags:
 !!! info "Disclaimer"
     Not all terminals support RGB, HSL, or Hex colors. If your terminal does support such advanced colors, read on.
 
+## Full Text Functions
+
+::: colorist.print.foreground.hex.MkDocstringsWrapper
+    options:
+      show_category_heading: false
+      heading_level: 4
+      merge_init_into_class: true
+
+::: colorist.print.background.hex.MkDocstringsWrapper
+    options:
+      show_category_heading: false
+      heading_level: 4
+      merge_init_into_class: true
+
 ## Custom String Styling
 ### `ColorHex(hex)`
 ::: colorist.ColorHex
--- src/colorist/print/background/hex.py
+++ src/colorist/print/background/hex.py
@@ -1,11 +1,35 @@
 # Copyright 2022 – present, Jakob Bagterp. BSD 3-Clause license and refer to LICENSE file.
 
+from ...model.abc.mkdocstrings import MkDocstringsWrapper_ABC
 from ...model.background.hex import BgColorHex
 from ...print.general import print_color
 
 
 def bg_hex(text: str, hex: str) -> None:
-    """Prints full line of text on colored background defined in Hex color. Expects valid Hex color value, for instance #B4FBB8 or B4FBB8, #B4F or B4F."""
+    """Prints full line of text on colored background defined in Hex color. Expects valid Hex color value, for instance `#B4FBB8` or `B4FBB8`, `#B4F` or `B4F`."""
 
     bg_color_hex = BgColorHex(hex)
     print_color(text, bg_color=bg_color_hex)
+
+
+class MkDocstringsWrapper(MkDocstringsWrapper_ABC):
+    def bg_hex(self, text: str, hex: str) -> None:
+        """Prints full line of text on colored background defined in Hex color.
+
+        Args:
+            text (str): Text to be printed on colored background.
+            hex (str): Hex color value, for instance `#B4FBB8` or `B4FBB8`, `#B4F` or `B4F`.
+
+        Example:
+            ```python linenums="1"
+            from colorist import hex, bg_hex
+
+            hex("I want this text in coral Hex colors", "#ff7f50")
+            bg_hex("I want this background in coral Hex colors", "#ff7f50")
+            ```
+
+            How it appears in the terminal:
+
+            ![Example of text in Hex colors printed in a terminal window](../assets/images/examples/hex_full_text.png)
+        """
+        # TODO: Update code example to only use bg_hex() and not hex().
