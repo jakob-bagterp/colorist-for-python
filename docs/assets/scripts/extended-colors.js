a0bc1bd8a60feb78f447944d6ae620e8b490cd59
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-19 21:26:23 +0200

    Refactored and simplified extended colors script so we don't convert back and forth between RGB and Hex – we can keep everything in RGB.

--- docs/assets/scripts/extended-colors.js
+++ docs/assets/scripts/extended-colors.js
@@ -1,54 +1,32 @@
-// Palette from colors.css converted from RGB to Hex:
-const _black = '#030501';
-const _brightWhite = '#fefefe';
+class RgbaColor {
+    constructor(rgba) {
+        this.rgba = rgba;
+        let [red, green, blue, alpha] = rgba.match(/\d+/g).map(Number);
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha !== undefined ? alpha / 255 : 1; // Normalize alpha to range [0, 1].
+    }
+}
 
-function getTextColorFromBackgroundColor(hexColor) {
-    function normalizeHexColorValue(hexColor) {
-        // If the hex color code has a leading #, remove it, e.g. #0099CC to 0099CC.
-        if (hexColor.slice(0, 1) === '#') {
-            hexColor = hexColor.slice(1);
-        }
+function getTextHexColorInContrastToBackgroundColor(rgbaColor) {
+    // Palette from colors.css converted from RGB to Hex:
+    const _black = '#030501';
+    const _brightWhite = '#fefefe';
 
-        // If a hexcode has 3 charaters, convert it to 6 characters, e.g. 09C to 0099CC.
-        if (hexColor.length === 3) {
-            hexColor = hexColor.split('').map(function (hex) {
-                return `${hex}${hex}`;
-            }).join('');
-        }
-        return hexColor;
+    function getYiqRatio(rgbaColor) {
+        return ((rgbaColor.red * 299) + (rgbaColor.green * 587) + (rgbaColor.blue * 114)) / 1000;
     }
 
-    function convertHexColorToRgb(hexColor) {
-        let red = parseInt(hexColor.substr(0, 2), 16);
-        let green = parseInt(hexColor.substr(2, 2), 16);
-        let blue = parseInt(hexColor.substr(4, 2), 16);
-        return { red, green, blue };
-    }
-
-    function getYiqRatioFromRgb(red, green, blue) {
-        return ((red * 299) + (green * 587) + (blue * 114)) / 1000;
-    }
-
-    hexColor = normalizeHexColorValue(hexColor);
-    let { red, green, blue } = convertHexColorToRgb(hexColor);
-	let yiqRatio = getYiqRatioFromRgb(red, green, blue);
+	let yiqRatio = getYiqRatio(rgbaColor);
 	return (yiqRatio >= 128) ? _black : _brightWhite;
 };
 
 function setTextColorForTableCells() {
-    function rgbToHex(rgb) {
-        let result = rgb.match(/\d+/g).map(function(num) {
-            let hex = parseInt(num).toString(16);
-            return hex.length === 1 ? '0' + hex : hex;
-        });
-        return `#${result.join('')}`;
-    }
-
-    document.querySelectorAll('td.extended-colors').forEach(function(td) {
-        let backgroundColor = window.getComputedStyle(td).backgroundColor;
-        let hexColor = rgbToHex(backgroundColor);
-        let textColor = getTextColorFromBackgroundColor(hexColor);
-        td.style.color = textColor;
+    document.querySelectorAll('td.extended-colors').forEach(function(tdCell) {
+        let backgroundColor = new RgbaColor(window.getComputedStyle(tdCell).backgroundColor);
+        let textColorHex = getTextHexColorInContrastToBackgroundColor(backgroundColor);
+        tdCell.style.color = textColorHex;
     });
 }
 
