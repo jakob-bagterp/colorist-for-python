0f01145988ff3a5c790c176eaea144284918d6c7
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-11 18:28:16 +0200

    Bump to version 1.8.1.

--- src/colorist/version.py
+++ src/colorist/version.py
@@ -1,4 +1,4 @@
 # Copyright 2022 – present, Jakob Bagterp. BSD 3-Clause license and refer to LICENSE file.
 
-__version_info__ = (1, 8, 0)
+__version_info__ = (1, 8, 1)
 __version__ = ".".join(map(str, __version_info__))
