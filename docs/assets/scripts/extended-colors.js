75aabde0b2507e93f6ab4f821be2511537d9b0db
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-11-25 10:38:55 +0100

    Minor updated comments.

--- docs/assets/scripts/extended-colors.js
+++ docs/assets/scripts/extended-colors.js
@@ -1,4 +1,5 @@
 // Palette from colors.css converted from RGB to Hex:
+
 const BLACK = '#030501';
 const BRIGHT_WHITE = '#fefefe';
 
@@ -9,7 +10,7 @@ class RgbaColor {
         this.red = red;
         this.green = green;
         this.blue = blue;
-        this.alpha = alpha !== undefined ? alpha / 255 : 1; // Normalize alpha to range [0, 1].
+        this.alpha = alpha !== undefined ? alpha / 255 : 1;  // Normalize alpha to range [0, 1].
     }
 }
 
